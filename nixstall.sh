e4c83634fed82b04cc970d9ab6e0dd5fd4249e18
sidharthify <wednisegit@gmail.com>
2025-10-16 17:41:48 +0530

    nixstall: Check for inputs
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- nixstall.sh
+++ nixstall.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env zsh
 set -e
 
-PACKAGES_PATH="/etc/nixos/packages/packages.nix"
+PACKAGES="/etc/nixos/packages/packages.nix"
 
 # colors
 RED='\033[0;31m'
 BLUE='\033[1;34m'
+GREEN='\033[0;32m'
 NC='\033[0m'
+
+package=$1
+
+# check input
+if [[ -z "$package" ]]; then
+  echo -e "${BLUE}Nixstall:${NC} ${RED}ERROR:${NC} Usage: $0 <package-name>"
+  exit 1
